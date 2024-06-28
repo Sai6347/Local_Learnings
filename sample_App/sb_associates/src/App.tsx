d09a1949be46af0f9cb1d98782c78f093d244c8c
sai6347 <saibharath7066@gmail.com>
2024-06-28 16:28:43 +0530

    added Typographyatom for sb

--- sample_App/sb_associates/src/App.tsx
+++ sample_App/sb_associates/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import TypographyAtom from './components/Atoms';
 
 function App() {
   return (
    <>
     <h2>Welcome to SB_ASSOCIATES</h2>
+    <TypographyAtom variant='body1' children="we help you to achieve your Goals."/>
    </>
   );
 }
